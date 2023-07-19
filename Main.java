da4e83163699bd3c0a5d97386eefad1ec1fad224
HUGO CHAVEZ <132840432+hchavezg1@users.noreply.github.com>
2023-07-18 22:52:28 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int resultado = suma(10, 10, 10);
+
+        System.out.println(resultado);
+    }
+
+    public static int suma(int a, int b, int c){
+        return a + b + c;
+
+
+    }
+
+}
